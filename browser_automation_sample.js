743b83afea9648c7a9ac18cc4c26ff2a81416c42
Axel Hodler <axel@hodler.co>
2017-07-02 11:43:02 +0200

    support macos and ubuntu

--- browser_automation_sample.js
+++ browser_automation_sample.js
@@ -3,10 +3,16 @@ var chrome = require('selenium-webdriver/chrome')
 var path = require('chromedriver').path
 var By = webdriver.By
 var until = webdriver.until
-var macosUsername = process.argv[2]
+var username = process.argv[2]
+var os = require('os')
 
 var options = new chrome.Options()
-options.addArguments('user-data-dir=/Users/' + macosUsername + '/Library/Appliction Support/Google/Chrome')
+if (os.platform() === 'darwin') {
+  options.addArguments('user-data-dir=/Users/' + username + '/Library/Appliction Support/Google/Chrome')
+} else {
+  options.addArguments('user-data-dir=/home/' + username + '/.config/google-chrome')
+}
+
 var driver = new webdriver.Builder()
   .withCapabilities(webdriver.Capabilities.chrome())
   .setChromeOptions(options)
